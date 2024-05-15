2c6bac81a43b9f7624d3da162cfcb32cba99afb5
kvvo <145585497+kvvo@users.noreply.github.com>
2024-05-15 09:15:41 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,6 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+// data stored in csv folder on GitHub, not Google sheets
+var googleDocURL = null;
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
