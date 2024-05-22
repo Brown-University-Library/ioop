8daac62f3220f824f16c882cac67112b6e3755a4
kvvo <145585497+kvvo@users.noreply.github.com>
2024-05-22 14:07:18 -0400

    Delete google-doc-url.js

--- google-doc-url.js
+++
@@ -1,6 +0,0 @@
-// paste in your published Google Sheets URL from the browser address bar
-// data stored in csv folder on GitHub, not Google sheets
-var googleDocURL = null;
-
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
