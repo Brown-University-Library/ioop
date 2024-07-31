3024dd746286ea2d49c4d28dd69cee36177308a3
kvvo <145585497+kvvo@users.noreply.github.com>
2024-07-31 15:08:53 -0400

    Update timeslider.js

--- src/timeslider.js
+++ src/timeslider.js
@@ -1,59 +1,71 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sheet = document.createElement('style');
-    document.body.appendChild(sheet);
+document.addEventListener("DOMContentLoaded", function () {
+  var sheet = document.createElement("style");
+  document.body.appendChild(sheet);
 
-    var $rangeInput = document.querySelector('.range input');
-    var rangeLabels = document.querySelectorAll('.range-labels li');
-    var prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
+  var $rangeInput = document.querySelector(".range input");
+  var rangeLabels = document.querySelectorAll(".range-labels li");
+  var prefs = ["webkit-slider-runnable-track", "moz-range-track", "ms-track"];
 
-    var getTrackStyle = function(el) {
-        var curVal = el.value;
-        var val = (curVal - 0) * 16.666666667; // Updated to match the range of values (0-6)
-        var style = '';
+  var getTrackStyle = function (el) {
+    var curVal = el.value;
+    var val = (curVal - 0) * 16.666666667; // Updated to match the range of values (0-6)
+    var style = "";
 
-        // Set active label
-        rangeLabels.forEach(function(label) {
-            label.classList.remove('active', 'selected');
-        });
-
-        var curLabel = rangeLabels[curVal];
-        if (curLabel) {
-            curLabel.classList.add('active', 'selected');
-            for (var i = 0; i < curVal; i++) {
-                rangeLabels[i].classList.add('selected');
-            }
-        }
-
-        // Change background gradient
-        for (var i = 0; i < prefs.length; i++) {
-            style += '.range {background: linear-gradient(to right, #37adbf 0%, #37adbf ' + val + '%, #fff ' + val + '%, #fff 100%)}';
-            style += '.range input::-' + prefs[i] + '{background: linear-gradient(to right, #37adbf 0%, #37adbf ' + val + '%, #636262 ' + val + '%, #636262 100%)}';
-        }
-
-        return style;
-    };
-
-    $rangeInput.addEventListener('input', function() {
-        sheet.textContent = getTrackStyle(this);
+    // Set active label
+    rangeLabels.forEach(function (label) {
+      label.classList.remove("active", "selected");
     });
 
-    // Change input value on label click
-    rangeLabels.forEach(function(label, index) {
-        label.addEventListener('click', function() {
-            $rangeInput.value = index;
-            $rangeInput.dispatchEvent(new Event('input'));
-        });
-    });
+    var curLabel = rangeLabels[curVal];
+    if (curLabel) {
+      curLabel.classList.add("active", "selected");
+      for (var i = 0; i < curVal; i++) {
+        rangeLabels[i].classList.add("selected");
+      }
+    }
 
-    // Initialize the style
-    sheet.textContent = getTrackStyle($rangeInput);
+    // Change background gradient
+    for (var i = 0; i < prefs.length; i++) {
+      style +=
+        ".range {background: linear-gradient(to right, #37adbf 0%, #37adbf " +
+        val +
+        "%, #fff " +
+        val +
+        "%, #fff 100%)}";
+      style +=
+        ".range input::-" +
+        prefs[i] +
+        "{background: linear-gradient(to right, #37adbf 0%, #37adbf " +
+        val +
+        "%, #636262 " +
+        val +
+        "%, #636262 100%)}";
+    }
+
+    return style;
+  };
+
+  $rangeInput.addEventListener("input", function () {
+    sheet.textContent = getTrackStyle(this);
+  });
+
+  // Change input value on label click
+  rangeLabels.forEach(function (label, index) {
+    label.addEventListener("click", function () {
+      $rangeInput.value = index;
+      $rangeInput.dispatchEvent(new Event("input"));
+    });
+  });
+
+  // Initialize the style
+  sheet.textContent = getTrackStyle($rangeInput);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var timeSlider = document.querySelector('.range input');
-    timeSlider.addEventListener('input', function() {
-        var selectedTime = parseInt(this.value, 10);
-        console.log("Selected time:", selectedTime);
-        filterDataPoints(selectedTime);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var timeSlider = document.querySelector(".range input");
+  timeSlider.addEventListener("input", function () {
+    var selectedTime = parseInt(this.value, 10);
+    console.log("Selected time:", selectedTime);
+    filterDataPoints(selectedTime);
+  });
 });
